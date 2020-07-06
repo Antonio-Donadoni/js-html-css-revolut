71a274f7b389f31569673a6d2a7c67154048fb1b
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-06 16:26:01 +0200

    ADD JS NAVBAR

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("ciao");
+$(document).ready(function(){
+  $(".dropdown-parent").mouseenter(function(){
+    $(this).children(".dropdown").css("display", "block");
+    $(this).children("a").css("color", "black");
+  });
+  $(".dropdown-parent").mouseleave(function(){
+    $(this).children(".dropdown").css("display", "none");
+    $(this).children("a").css("color", "#555");
+  });
+
+
+});
