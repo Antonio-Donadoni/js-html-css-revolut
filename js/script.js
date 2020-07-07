e3621ebc09a49f758b4691905317143c5e29dfbb
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-07 10:08:13 +0200

    ADD menu hamburger

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
-  $(".dropdown-parent").mouseenter(function(){
+
+  $("nav .dropdown-parent").mouseenter(function(){
     $(this).children(".dropdown").css("display", "block");
     $(this).children("a").css("color", "black");
   });
-  $(".dropdown-parent").mouseleave(function(){
+
+  $("nav .dropdown-parent").mouseleave(function(){
     $(this).children(".dropdown").css("display", "none");
     $(this).children("a").css("color", "#555");
   });
 
+  $(".fa-bars").click(function(){
+    $(".hamburger-menu").addClass("active");
+
+  });
+
+  $(".fa-times").click(function(){
+    $(".hamburger-menu").removeClass("active");
+
+  });
+
+  var active = false
+    $(".hamburger-menu .dropdown-parent").click(function(){
+      if (active) {
+        $(this).children(".dropdown-click").slideUp();
+        $(this).children("a").css("color", "#555");
+        active = false;
+      }
+      else {
+      $(this).children(".dropdown-click").slideDown();
+      $(this).children("a").css("color", "black");
+      active = true;
+      }
+    });
 
 });
