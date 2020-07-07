640433096a615ef6da80702f5efea00c1d7dcf25
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-07 13:09:52 +0200

    ADD JS COMMENT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  // JS DROPDOWN DESKTOP
   $("nav .dropdown-parent").mouseenter(function(){
     $(this).children(".dropdown").show();
     $(this).children("a").addClass("hover");
@@ -10,17 +10,19 @@ $(document).ready(function(){
     $(this).children("a").removeClass("hover");
   });
 
+// HAMBURGER CLICK
   $(".menu-toggle").click(function(){
     $(".hamburger-menu").toggleClass("active");
     $(this).children("i").toggle();
   });
 
-
+// SLIDE MENU MOBILE
   $(".hamburger-menu .dropdown-parent").click(function(){
     $(this).children(".dropdown-click").slideToggle();
     $(this).children("a").toggleClass("hover");
   });
 
+// HOVER SUI LINK DESKTOP/MOBILE
   $(".dropdown-click a, .dropdown a").mouseenter(function(){
     $(this).addClass("hover");
   });
